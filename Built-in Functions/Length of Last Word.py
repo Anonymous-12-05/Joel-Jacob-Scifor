fefc0502fda562f059824e8e471fb3d286801a67
Joel Jacob John <achuthengone03@gmail.com>
2023-10-29 13:42:15 +0530

    Delete Built-in Functions directory

--- Built-in Functions/Length of Last Word.py
+++
@@ -1,4 +0,0 @@
-#https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=programming-skills
-def lengthOfLastWord(self, s: str) -> int:
-        l=s.split()
-        return len(l[len(l)-1])
--- Built-in Functions/To Lower Case.py
+++
@@ -1,3 +0,0 @@
-#https://leetcode.com/problems/to-lower-case/?envType=study-plan-v2&envId=programming-skills
-def toLowerCase(self, s: str) -> str:
-        return s.lower()
