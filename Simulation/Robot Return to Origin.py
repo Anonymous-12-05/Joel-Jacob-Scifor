d13a5a0ff684e2c37908e2f787455cc9eb0cbc4d
Joel Jacob John <achuthengone03@gmail.com>
2023-10-29 13:42:40 +0530

    Delete Simulation directory

--- Simulation/Robot Return to Origin.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/robot-return-to-origin/description/?envType=study-plan-v2&envId=programming-skills
-class Solution:
-    def judgeCircle(self, moves: str) -> bool:
-        d=dict()
-        for i in moves:
-            d[i]=d.get(i,0)+1
-        if(d.get("U",0)==d.get("D",0) and d.get("L",0)==d.get("R",0)):
-            return True
-        return False
